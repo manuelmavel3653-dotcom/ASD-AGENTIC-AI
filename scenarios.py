1ffc43be4697128a1b018f4e8853c47263eb23e7
manuelmavel3653-dotcom <manuelmavel3653@gmail.com>
2026-05-30 22:24:15 +0800

    Update scenarios.py

--- scenarios.py
+++ scenarios.py
@@ -1,1 +1,7 @@
-
+SCENARIOS = [
+    "An ASD student keeps walking on a wet floor.",
+    "An ASD student does not understand a teacher's facial expression.",
+    "An ASD student becomes confused by long instructions.",
+    "An ASD student repeatedly asks the same question.",
+    "An ASD student becomes stressed during communication."
+]
